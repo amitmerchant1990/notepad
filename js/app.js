901f33d8336a1e0c815e6e54bf4d1601287658dc
Amit Merchant <bullredeyes@gmail.com>
2016-07-23 01:03:40 +0530

    Add ServiceWorker functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-  $('#note').on('keyup', function(){
-    console.log($(this).val());
+  $('#note').bind('input propertychange', function(){
     localStorage.setItem("note", $(this).val());
-  })
+  });
+
+  if(localStorage.getItem("note") && localStorage.getItem("note")!=''){
+    var noteItem = localStorage.getItem("note")
+    $('#note').val(noteItem);
+  }
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
---
+++ sw.js
@@ -0,0 +1,63 @@
+importScripts('js/cache-polyfill.js');
+
+var CACHE_VERSION = 'app-v4';
+var CACHE_FILES = [
+    '/',
+    'index.html',
+    'js/app.js',
+    'js/jquery.min.js',
+    'js/bootstrap.min.js',
+    'css/bootstrap.min.css',
+    'css/style.css'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function(res){
+            if(res){
+                return res;
+            }
+            requestBackend(event);
+        })
+    )
+});
+
+function requestBackend(event){
+    var url = event.request.clone();
+    return fetch(url).then(function(res){
+        //if not a valid response send the error
+        if(!res || res.status !== 200 || res.type !== 'basic'){
+            return res;
+        }
+
+        var response = res.clone();
+
+        caches.open(CACHE_VERSION).then(function(cache){
+            cache.put(event.request, response);
+        });
+
+        return res;
+    })
+}
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function(keys){
+            return Promise.all(keys.map(function(key, i){
+                if(key !== CACHE_VERSION){
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+});
