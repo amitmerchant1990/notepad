7a7447244e9a061653c32090ab3afdff3f75fd75
Amit Merchant <bullredeyes@gmail.com>
2017-05-10 00:18:17 +0530

    Fixes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
-  $('#note').bind('input propertychange', function(){
+  $('#note').keyup(debounce(function(){
     localStorage.setItem("note", $(this).val());
-  });
+    console.log('dbounce test..');
+    $("#flash").removeClass('hidden');
+  },500));
 
   if(localStorage.getItem("note") && localStorage.getItem("note")!=''){
     var noteItem = localStorage.getItem("note")
     $('#note').val(noteItem);
   }
 });
+
+setInterval(function() {
+      $("#flash").addClass('hidden');
+      //alert('test');
+  }, 5000);
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 // Registering ServiceWorker
 if ('serviceWorker' in navigator) {
