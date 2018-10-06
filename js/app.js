d1b740a091cba70e5b95246488eb0b790377049e
amitmerchant1990 <bullredeyes@gmail.com>
2018-10-06 21:43:45 +0530

    Implement dark mode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#note').keyup(debounce(function(){
-    localStorage.setItem("note", $(this).val());
+    localStorage.setItem('note', $(this).val());
   },500));
 
   let initialText = `This is an offline-capable Notepad which is based on the ServiceWorker.
@@ -10,16 +10,27 @@ $(document).ready(function(){
     - Write notes which then saved to the localStorage.
     - Installable on supported browsers for offline usage.
     - "Add To Home Screen" feature on Android supported devices to launch the app from home screen.
+    - Dark mode.
     - Privacy focused - We'll never collect your precious data.
     - Light-weight - Loads almost instantly.
   
   ** Start writing your notes **`;
 
-  if(localStorage.getItem("note") && localStorage.getItem("note")!=''){
-    var noteItem = localStorage.getItem("note")
+  if(localStorage.getItem('note') && localStorage.getItem('note')!=''){
+    var noteItem = localStorage.getItem('note');
     $('#note').val(noteItem);
   } else {
     $('#note').val(initialText);
+  }
+
+  if(localStorage.getItem('mode') && localStorage.getItem('mode')!=''){
+    if(localStorage.getItem('mode') == 'dark'){
+      $(document.body).addClass('dark');
+      $('#mode').html('☀️');
+    }else{
+      $(document.body).removeClass('dark');
+      $('#mode').html('🌘');
+    }
   }
 
   $('#clearNotes').on('click', function(){
@@ -31,6 +42,19 @@ $(document).ready(function(){
   $('.cookie_box_close').click(function(){
     $('.adFooter').animate({opacity:0 }, "slow");
     return false;
+  });
+
+  $('#mode').click(function(){
+    $(document.body).toggleClass('dark');
+    let bodyClass = $(document.body).attr('class');
+
+    if(bodyClass == 'dark'){
+      localStorage.setItem('mode', 'dark');
+      $(this).html('☀️');
+    } else {
+      localStorage.setItem('mode', 'light');
+      $(this).html('🌘');
+    }
   });
 
 });
