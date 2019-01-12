056a070f1b314efb1025371bb69521645c871839
Amit Merchant <bullredeyes@gmail.com>
2019-01-12 11:09:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,63 +1,73 @@
 $(document).ready(function(){
-  $('#note').keyup(debounce(function(){
-    localStorage.setItem('note', $(this).val());
-  },500));
+	$('#note').keyup(debounce(function(){
+		localStorage.setItem('note', $(this).val());
+	},500));
 
-  let initialText = `This is an offline-capable Notepad which is based on the ServiceWorker.
+	let initialText = `This is an offline-capable Notepad which is based on the ServiceWorker.
 
-  The app serves following features:
-    
-    - Write notes which then saved to the localStorage.
-    - Installable on supported browsers for offline usage.
-    - "Add To Home Screen" feature on Android supported devices to launch the app from home screen.
-    - Dark mode.
-    - Privacy focused - We'll never collect your precious data.
-    - Light-weight - Loads almost instantly.
-  
-  ** Start writing your notes **`;
-  let darkmodeText = 'Enable dark mode';
-  let lightmodeText = 'Enable light mode';
+	The app serves following features:
 
-  if(localStorage.getItem('note') && localStorage.getItem('note')!=''){
-    var noteItem = localStorage.getItem('note');
-    $('#note').val(noteItem);
-  } else {
-    $('#note').val(initialText);
-  }
+	- Write notes which then saved to the localStorage.
+	- Installable on supported browsers for offline usage.
+	- "Add To Home Screen" feature on Android supported devices to launch the app from home screen.
+	- Dark mode.
+	- Privacy focused - We'll never collect your precious data.
+	- Light-weight - Loads almost instantly.
 
-  if(localStorage.getItem('mode') && localStorage.getItem('mode')!=''){
-    if(localStorage.getItem('mode') == 'dark'){
-      $(document.body).addClass('dark');
-      $('#mode').html('☀️').attr('title', lightmodeText);
-    }else{
-      $(document.body).removeClass('dark');
-      $('#mode').html('🌘').attr('title', darkmodeText);
-    }
-  }
-
-  $('#clearNotes').on('click', function(){
-    $('#note').val('').focus();
-    localStorage.setItem("note", '');
-  });
+	** Start writing your notes **`;
 	
+	let darkmodeText = 'Enable dark mode';
+	let lightmodeText = 'Enable light mode';
+
+	if(localStorage.getItem('note') && localStorage.getItem('note')!=''){
+	var noteItem = localStorage.getItem('note');
+		$('#note').val(noteItem);
+	} else {
+		$('#note').val(initialText);
+	}
+
+	if(localStorage.getItem('mode') && localStorage.getItem('mode')!=''){
+		if(localStorage.getItem('mode') == 'dark'){
+			$(document.body).addClass('dark');
+			$('#mode').html('☀️').attr('title', lightmodeText);
+		}else{
+			$(document.body).removeClass('dark');
+			$('#mode').html('🌘').attr('title', darkmodeText);
+		}
+	}
+
+	$('#clearNotes').on('click', function(){
+		$('#note').val('').focus();
+			localStorage.setItem("note", '');
+		});
+
+		$('.cookie_box_close').click(function(){
+		$('.adFooter').animate({opacity:0 }, "slow");
+			return false;
+		});
+
+		$('#mode').click(function(){
+		$(document.body).toggleClass('dark');
+		let bodyClass = $(document.body).attr('class');
+
+		if(bodyClass == 'dark'){
+			localStorage.setItem('mode', 'dark');
+			$(this).html('☀️').attr('title', lightmodeText);
+		} else {
+			localStorage.setItem('mode', 'light');
+			$(this).html('🌘').attr('title', darkmodeText);
+		}
+	});
 	
-  $('.cookie_box_close').click(function(){
-    $('.adFooter').animate({opacity:0 }, "slow");
-    return false;
-  });
-
-  $('#mode').click(function(){
-    $(document.body).toggleClass('dark');
-    let bodyClass = $(document.body).attr('class');
-
-    if(bodyClass == 'dark'){
-      localStorage.setItem('mode', 'dark');
-      $(this).html('☀️').attr('title', lightmodeText);
-    } else {
-      localStorage.setItem('mode', 'light');
-      $(this).html('🌘').attr('title', darkmodeText);
-    }
-  });
+	window.matchMedia('(prefers-color-scheme: dark)').addListener(({ matches }) => {
+		if (matches) {
+			$(document.body).addClass('dark');
+			localStorage.setItem('mode', 'dark');
+		}else{
+			$(document.body).removeClass('dark');
+			localStorage.setItem('mode', 'light');
+		}
+	});
 
 });
 
@@ -76,23 +86,13 @@ function debounce(func, wait, immediate) {
 	};
 };
 
-window.matchMedia('(prefers-color-scheme: dark)').addListener(({ matches }) => {
-	if (matches) {
-		$(document.body).addClass('dark');
-		localStorage.setItem('mode', 'dark');
-	}else{
-		$(document.body).removeClass('dark');
-		localStorage.setItem('mode', 'light');
-	}
-});
-
 // Registering ServiceWorker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
+	navigator.serviceWorker.register('sw.js').then(function(registration) {
+		// Registration was successful
+		console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+	}).catch(function(err) {
+		// registration failed :(
+		console.log('ServiceWorker registration failed: ', err);
+	});
 }
