7152616f9ae7f2b7f96b9c7b212116d69396ffdd
Hitesh Balar <hsbalar@gmail.com>
2022-08-28 16:50:09 +0530

    Redesign selector & remove tab feature

---
+++ js/selector.js
@@ -0,0 +1,64 @@
+function getState(state) {
+    return localStorage.getItem(state);
+}
+
+function setState(state, value) {
+    localStorage.setItem(state, value);
+}
+
+function removeState(state) {
+    localStorage.removeItem(state);
+}
+
+function selectById(id) {
+    return $(`#${id}`);
+}
+
+function selectByClassName(className) {
+    return $(`.${className}`);
+}
+
+function get(context) {
+    return $(context);
+}
+
+function selector() {
+    return {
+        notepad: {
+            aboutModal: selectById('aboutModal'),
+            clearNotes: selectById('clearNotes'),
+            closeDonationPopup: selectById('closeDonationPopup'),
+            copyToClipboard: selectById('copyToClipboard'),
+            downloadNotes: selectById('downloadNotes'),
+            fontSize: selectById('fontSize'),
+            fontWeight: selectById('fontWeight'),
+            installApp: selectById('installApp'),
+            lineHeight: selectById('lineHeight'),
+            mode: selectById('mode'),
+            note: selectById('note'),
+            preferencesModal: selectById('preferencesModal'),
+            resetPreferences: selectById('resetPreferences'),
+            showWordCountPill: selectById('showWordCountPill'),
+            wordCount: selectById('wordCount'),
+            installAppButtonContainer: selectByClassName('install-app-btn-container'),
+            stickyNotice: selectByClassName('sticky-notice'),
+            wordCountContainer: selectByClassName('word-count-container'),
+        },
+        state: {
+            note: getState('note'),
+            mode: getState('mode'),
+            isUserPreferredTheme: getState('isUserPreferredTheme'),
+            userChosenFontSize: getState('userChosenFontSize'),
+            userChosenFontWeight: getState('userChosenFontWeight'),
+            userChosenLineHeight: getState('userChosenLineHeight'),
+            hasUserDismissedDonationPopup: getState('hasUserDismissedDonationPopup'),
+            userChosenWordCountPillSelected: getState('userChosenWordCountPillSelected'),
+        },
+        get,
+        getState,
+        setState,
+        removeState,
+        selectById,
+        selectByClassName
+    }
+}
