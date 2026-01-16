6081b1009025812a85e2d109d2fe46aabd4c0e10
Amit Merchant <bullredeyes@gmail.com>
2026-01-16 19:37:29 +0530

    tasks: fix a focus related issue

--- tasks/js/app.js
+++ tasks/js/app.js
@@ -61,6 +61,7 @@ $(document).ready(function() {
         saveTasks();
         $('#taskInput').val('');
         toggleNoTasksMessage(); 
+        $('#taskInput').focus();
     }
 
     // Event delegation for marking as complete and deleting
